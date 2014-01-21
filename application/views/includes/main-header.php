18d43b10e3660619566063f3b1e88ce8ace099ef
googaloo <rustin.l.odom@gmail.com>
2014-01-20 18:27:16 -0700

    Start on the header / Fonts / icon-fonts, etc

--- application/views/includes/main-header.php
+++ application/views/includes/main-header.php
@@ -1,19 +1,67 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
-	
+
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-	<title>Amp My BIZZZZZ</title>
+	<title>Amp My Biz Up!</title>
 
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/normalize.css'); ?>">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/foundation.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/foundation-icons.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/styles.css'); ?>">
 
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/modernizr.js'); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/foundation.min.js'); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url('assets/js/script.js');?>"></script>
+	<script type="text/javascript">$(document).foundation();</script>
 
 </head>
 
 <body>
+
+<!-- =============== -->
+<!-- ===== NAV ===== -->
+<!-- =============== -->
+
+<!-- SMALL profile/logo nav -->
+<div class="off-canvas-wrap top-nav">
+	<div class="inner-wrap">
+		<a class="left-off-canvas-toggle hide-for-large left top-nav-list-button"><i class="fi-list large-icon"></i></a>
+		<img class="top-nav-logo" src="http://www.fillmurray.com/115/30" alt="Our Logo">
+
+		<!-- Mobile Offcavas menu -->
+		<aside class="left-off-canvas-menu">
+
+			<div class="row">
+				<div class="small-11 small-centered columns courses-bg">Courses</div>
+				<div class="small-11 small-centered columns community-bg">Community</div>
+				<div class="small-11 small-centered columns earn-points-bg">Earn Points</div>
+				<div class="small-11 small-centered columns spend-points-bg">Spend Points</div>
+				<div class="small-11 small-centered columns blog-bg">Blog</div>
+			</div>
+
+		</aside>
+
+			<div class="top-nav-container right">
+
+				<ul class="top-nav-page-links left hide-for-medium-down">
+					<li class="left nav-courses nav-pages">Courses</li>
+					<li class="left nav-community nav-pages">Community</li>
+					<li class="left nav-earn-points nav-pages">Earn Points</li>
+					<li class="left nav-spend-points nav-pages">Spend Points</li>
+					<li class="left nav-blog nav-pages">Blog</li>
+				</ul>
+
+				<div class="top-nav-profile-container right">
+
+					<div class="profile-nav-user-name right">Company B</div>
+					<a href="#" class="top-nav-profile-avatar"><img src="http://www.fillmurray.com/35/35" width="50" height="50" class="circular" alt=""></a>
+
+				</div>
+
+			</div>
+		<a class="exit-off-canvas"></a>
+	</div>
+</div>
