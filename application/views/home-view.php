ae7ae9484be00fb00e0eed9cff504c8af5f95220
googaloo <rustin.l.odom@gmail.com>
2014-02-18 13:06:27 -0700

    added new banner - centered

--- application/views/home-view.php
+++ application/views/home-view.php
@@ -2,13 +2,9 @@
 
 	<div class="svg-container">
 
-		<object class="home-intro" type="image/svg+xml" data="<?php echo base_url('assets/images'); ?>/home-intro.svg" width="4000" height="800"></object>
+		<object style="margin: 0 auto; display:block;" class="home-intro" type="image/svg+xml" data="<?php echo base_url('assets/images'); ?>/lab-banner.svg"></object>
+
 	</div>
-</div>
-
-<!--
-
-<div class="home-above-fold-container">
 
 	<div class="row">
 
@@ -20,7 +16,7 @@
 
 	<div class="row">
 
-		<div class="large-5 large-offset-0 medium-5 medium-offset-1 columns">
+		<div class="large-4 large-offset-0 medium-5 medium-offset-1 columns left">
 			<h2 class="subheader">It takes a village to grow a business.</h2>
 			<p class="home-above-fold-description white-text">Williamsburg sed farm-to-table slow-carb distillery. Deep v cray dolor, delectus officia ullamco chambray. YOLO you probably haven't heard of them Helvetica sustainable Terry Richardson, Blue Bottle 3 wolf moon placeat irony photo booth. </p>
 			<span class="button left hide-for-medium-down">Try a Course</span>
@@ -29,14 +25,12 @@
 			<span class="button alert right small hide-for-large">Sign Up Free</span>
 		</div>
 
-		<div class="large-6 large-offset-1 columns home-above-fold-intro flex-video widescreen">
-			<iframe width="640" height="360" src="//www.youtube.com/embed/eD-T_ppdfUY" frameborder="0" allowfullscreen></iframe>
-		</div>
 
-		<div class="hide-for-medium-up">
+
+		<div class="large-1 right columns">
 			Watch Video
 		</div>
 
 	</div>
 
-</div> <!-- end .home-above-fold-container -->
+</div>
